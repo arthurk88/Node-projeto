7d19d216fc1aaac2967021e0ced3cb261952157b
Arthur Santana <43881656+arthurk88@users.noreply.github.com>
2020-05-14 13:21:48 -0300

    Criando painel com estrutura separada
    Criando painel com estrutura separada

--- routes/admin.js
+++ routes/admin.js
@@ -5,5 +5,8 @@ const lingue = require('../models/lingue/pt-br')
 router.get('/',(req,res) => {
     res.render('painel',{lingue:lingue})    
 })
+router.get('/configuracao',(req,res) => {
+    res.render('configuracao',{lingue:lingue})    
+})
 
 module.exports = router
